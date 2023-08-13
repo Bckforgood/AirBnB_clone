357298b2f16b04a61b1d7424f6c1367e6184b4ce
Alexus91 <mrfadlkamal@gmail.com>
2023-08-13 13:18:46 -0700

    files models/base_model.py , console.py tests/test_console.py  models/engine/file_storage.py ready to be checked and tested

--- models/state.py
+++ models/state.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
-
 class State(BaseModel):
     """State class"""
     name = ""
-
-
-def _init_(self, *args, **kwargs):
-    """class constructor"""
-    super()._init_(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """class constructor"""
+        super().__init__(*args, **kwargs)
