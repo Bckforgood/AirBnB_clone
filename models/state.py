ba49b940675fc9432a8851602f69aff2bd2a4133
Bckforgood <116983963+Bckforgood@users.noreply.github.com>
2023-08-13 16:01:23 +0100

    Delete models directory

--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-class State(BaseModel):
-    """State class"""
-    name = ""
-    def _init_(self, *args, **kwargs):
-        """class constructor"""
-        super()._init_(*args, **kwargs)
