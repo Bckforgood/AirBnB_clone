29d8e117d4426202937a27284f598ef82551d9ec
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-13 14:17:57 -0700

    try to fix pycodestyle

--- models/state.py
+++ models/state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
     """State class"""
     name = ""
-    def __init__(self, *args, **kwargs):
-        """class constructor"""
-        super().__init__(*args, **kwargs)
+
+
+def __init__(self, *args, **kwargs):
+    """class constructor"""
+    super().__init__(*args, **kwargs)
